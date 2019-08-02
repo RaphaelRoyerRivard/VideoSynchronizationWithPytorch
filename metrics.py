ecfdac844147c16d1e52d28b372e75c54cf7d423
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-08-02 11:42:34 -0400

    fixed bug in metrics

--- metrics.py
+++ metrics.py
@@ -36,13 +36,15 @@ class EmbeddingL2DistanceMetric(Metric):
         average_positive_dist = 0
         average_negative_dist = 0
         count = 0
-        for batch in self.embeddings:
-            for triplet in batch:
-                count += 1
-                average_positive_dist += (triplet[1] - triplet[0]).pow(2).sum()
-                average_negative_dist += (triplet[2] - triplet[0]).pow(2).sum()
+        for triplets in self.embeddings:
+            anchors = triplets[0]  # (batch_size, embedding_size)
+            positives = triplets[1]  # (batch_size, embedding_size)
+            negatives = triplets[2]  # (batch_size, embedding_size)
+            count += anchors.shape[0]
+            average_positive_dist += (positives - anchors).pow(2).sum()
+            average_negative_dist += (negatives - anchors).pow(2).sum()
         if count == 0:
-            return 0, 0
+            return 0, 0, 0
         average_positive_dist /= count
         average_negative_dist /= count
         return digit_precision(average_positive_dist.item(), 2), \
@@ -67,12 +69,18 @@ class EmbeddingCosineSimilarityMetric(Metric):
         average_positive_similarity = 0
         average_negative_similarity = 0
         count = 0
-        for batch in self.embeddings:
-            for triplet in batch:
-                count += 1
-                normalized_anchor = triplet[0] / torch.norm(triplet[0])
-                average_positive_similarity += normalized_anchor.dot(triplet[1] / torch.norm(triplet[1]))
-                average_negative_similarity += normalized_anchor.dot(triplet[2] / torch.norm(triplet[2]))
+        for triplets in self.embeddings:
+            anchors = triplets[0]  # (batch_size, embedding_size)
+            positives = triplets[1]  # (batch_size, embedding_size)
+            negatives = triplets[2]  # (batch_size, embedding_size)
+            batch_size = anchors.shape[0]
+            count += batch_size
+            for i in range(batch_size):
+                normalized_anchor = anchors[i] / torch.norm(anchors[i])
+                normalized_positive = positives[i] / torch.norm(positives[i])
+                normalized_negative = negatives[i] / torch.norm(negatives[i])
+                average_positive_similarity += normalized_anchor.dot(normalized_positive)
+                average_negative_similarity += normalized_anchor.dot(normalized_negative)
         if count == 0:
             return 0, 0
         average_positive_similarity /= count

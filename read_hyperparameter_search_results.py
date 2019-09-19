5cbbdd6c8fec49b72e17cb3d1d06312ebf821f84
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-09-19 14:38:32 -0400

    sorted list of hyperparameter search trainings

--- read_hyperparameter_search_results.py
+++ read_hyperparameter_search_results.py
@@ -2,6 +2,7 @@ import os
 
 if __name__ == '__main__':
     base_path = r"E:\Users\root\Projects\VideoSynchronizationWithPytorch\trainings\hyperparameter_search"
+    sorted_xp = []
     best_score = 0
     best_path = None
     for path, subfolders, files in os.walk(base_path):
@@ -9,14 +10,15 @@ if __name__ == '__main__':
             f = open(path + r"\result.txt", 'r')
             res = f.readline()
             f.close()
+            f = open(path + r"\config.json", 'r')
+            config = f.readline()
+            f.close()
             res = float(res)
+            sorted_xp.append((res, path.split('\\')[-1], config))
             if res > best_score:
                 best_score = res
                 best_path = path
 
-    if best_path is not None:
-        f = open(best_path + r"\config.json", 'r')
-        config = f.readline()
-        f.close()
-        print(f"Best result of {best_score} for training {best_path}")
-        print("Config:", config)
+    sorted_xp.sort(key=lambda tup: tup[0], reverse=True)
+    for xp in sorted_xp:
+        print(xp)

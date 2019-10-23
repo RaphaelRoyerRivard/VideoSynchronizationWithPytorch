15a0409fae9c975bf97a4f40aaab3830711ae1f0
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-10-23 15:41:07 -0400

    now sorting hyperparameter search results by all of their 3 values

--- read_hyperparameter_search_results.py
+++ read_hyperparameter_search_results.py
@@ -1,11 +1,12 @@
 import os
+import numpy as np
 
 if __name__ == '__main__':
     base_path = r"E:\Users\root\Projects\VideoSynchronizationWithPytorch\trainings\hyperparameter_search"
     failed_xp = []
-    sorted_xp = []
-    best_score = 0
-    best_path = None
+    sorted_xp = [[], [], []]
+    best_scores = [0, 0, 0]
+    best_paths = [None, None, None]
     for path, subfolders, files in os.walk(base_path):
         if "config.json" in files:
             f = open(path + r"\config.json", 'r')
@@ -14,13 +15,14 @@ if __name__ == '__main__':
             xp_id = path.split('\\')[-1]
             if "result.txt" in files:
                 f = open(path + r"\result.txt", 'r')
-                res = f.readline()
+                lines = f.readlines()
                 f.close()
-                res = float(res)
-                sorted_xp.append((res, xp_id, config))
-                if res > best_score:
-                    best_score = res
-                    best_path = path
+                for matrix_type, res in enumerate(lines):
+                    res = float(res)
+                    sorted_xp[matrix_type].append((res, xp_id, config))
+                    if res > best_scores[matrix_type]:
+                        best_scores[matrix_type] = res
+                        best_paths[matrix_type] = path
             else:
                 failed_xp.append((xp_id, config))
 
@@ -28,7 +30,9 @@ if __name__ == '__main__':
     for xp in failed_xp:
         print(xp)
 
-    sorted_xp.sort(key=lambda tup: tup[0], reverse=True)
-    print("Sorted XP")
-    for xp in sorted_xp:
-        print(xp)
+    for i, matrix_type in enumerate(["Distance", "Similarity", "Combination"]):
+        if len(sorted_xp[i]) > 0:
+            sorted_xp[i].sort(key=lambda tup: tup[0], reverse=True)
+            print(f"Sorted XP for {matrix_type} matrices. AVG: {np.array([x[0] for x in sorted_xp[i]]).mean()}")
+            for xp in sorted_xp[i]:
+                print(xp)
